f9542e31ad709e8494774c0da04c49faf977d479
shubhendu-repo <rajputshubhendu@gmail.com>
2021-10-25 21:25:51 +0530

    inital setup

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import './Counter.css'
+
+class Counter extends Component {
+
+    constructor() {
+        
+        super();  // to use this, we need to call super()
+        this.state = {
+            counter: 0
+        }
+        // bind method with class to make `this` available inside the method
+        this.increment = this.increment.bind(this); 
+    }
+
+    render() {
+        return(
+        <div className="counter">
+            <button onClick={this.increment} >+1</button>
+            <span className="count">{this.state.counter}</span>
+        </div>
+        )
+    }
+
+    increment() {
+        if (this.state.counter >= 10) {
+            this.setState(
+                {
+                    counter : 0
+                }
+                )
+        } else {
+        this.setState(
+            {
+                counter : this.state.counter + 1
+            }
+        )
+    }
+    }
+}
+
+/*class Increment extends Component {
+    render() {
+        console.log('increment');
+    }
+}*/
+
+export default Counter;
